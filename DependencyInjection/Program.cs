2e9d57f214b4163b691f141181e78cee4f413d6e
mfmstr <g.kharebava01@gmail.com>
2023-09-10 02:44:16 +0400

    Dependency Injection

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace DependencyInjection
+{
+    //Violation:
+
+    //public class SalaryCalculator
+    //{
+    //    public float CalculateSalary(int hoursWorked, float hourlyRate)
+    //        => hoursWorked * hourlyRate;
+    //}
+
+    //public class EmployeeDetails
+    //{
+    //    public int HoursWorked { get; set; }
+    //    public int HourlyRate { get; set; }
+    //    public float GetSalary()
+    //    {
+    //        var salaryCalculator = new SalaryCalculator();
+    //        return salaryCalculator.CalculateSalary(HoursWorked, HourlyRate);
+    //    }
+    //}
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeeDetailsModified = new EmployeeDetailsModified(new SalaryCalculatorModified());
+            var employeeDetailsModified2 = new EmployeeDetailsModified(new SalaryCalculatorModified2());
+
+            employeeDetailsModified.HourlyRate = 100;
+            employeeDetailsModified.HoursWorked = 100;
+            employeeDetailsModified2.HourlyRate = 100;
+            employeeDetailsModified2.HoursWorked = 100;
+            Console.WriteLine($"The Total Pay is {employeeDetailsModified.GetSalary()}");
+            Console.WriteLine($"The Total Pay is {employeeDetailsModified2.GetSalary()}");
+        }
+    }
+
+    public interface ISalaryCalculator
+    {
+        float CalculateSalary(int hoursWorked, float hourlyRate);
+    }
+
+    public class SalaryCalculatorModified : ISalaryCalculator
+    {
+        public float CalculateSalary(int hoursWorked, float hourlyRate) => hoursWorked * hourlyRate;
+    }
+
+    public class SalaryCalculatorModified2 : ISalaryCalculator
+    {
+        public float CalculateSalary(int hoursWorked, float hourlyRate) => hoursWorked * hourlyRate *10;
+    }
+
+    public class EmployeeDetailsModified
+    {
+        private readonly ISalaryCalculator _salaryCalculator;
+        public int HoursWorked { get; set; }
+        public int HourlyRate { get; set; }
+        public EmployeeDetailsModified(ISalaryCalculator salaryCalculator)
+        {
+            _salaryCalculator = salaryCalculator;
+        }
+        public float GetSalary()
+        {
+            return _salaryCalculator.CalculateSalary(HoursWorked, HourlyRate);
+        }
+    }
+}
---
+++ Injection/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Injection
+{
+    internal class Program
+    {
+
+        public interface IService
+        {
+            void Serve();
+        }
+
+        public class Service1 : IService
+        {
+            public void Serve() { Console.WriteLine("Service1 Called"); }
+        }
+
+        public class Service2 : IService
+        {
+            public void Serve()
+            {
+                Console.WriteLine("Service2 Called");
+                Console.WriteLine("Service2 Called");
+                Console.WriteLine("Service2 Called");
+                Console.WriteLine("Service2 Called");
+
+            }
+        }
+
+        public class Client
+        {
+            private IService _service;
+            public Client(IService service)
+            {
+                _service = service;
+            }
+            public void ServeMethod() { _service.Serve(); }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Service1 s1 = new Service1();
+            Service2 s2 = new Service2();
+
+            Client c1 = new Client(s1);
+            c1 = new Client(s1);
+            c1.ServeMethod();
+
+
+            Client c2 = new Client(s2);
+            c1 = new Client(s2);
+            c1.ServeMethod();
+
+        }
+    }
+}
